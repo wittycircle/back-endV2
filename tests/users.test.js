7466a8c86fa41f6336132a287de134f1b6ff69cf
Raphael Sequoya <sequoya@wittycircle.com>
2017-01-21 22:38:54 +0100

    fix route /share and associated model/controller

--- tests/users.test.js
+++ tests/users.test.js
@@ -2,9 +2,11 @@ const chakram = require('chakram'),
 expect = chakram.expect,
 	home = 'http://localhost:3000'
 
-describe("Get user share", function() {
-	it("should send social share", function () {
-		return chakram.get(home + "/share/9999999")
+describe ('Users test : ', function () {
+
+describe('Get user share', function() {
+	it('should send social share', function () {
+		return chakram.get(home + '/share/9999999')
 			.then((r) => {
 				expect(r.body.success).to.equal(false)
 				expect(r).to.have.status(200)
@@ -12,11 +14,24 @@ describe("Get user share", function() {
 	})
 });
 
-describe("Put user share", function () {
-	it("should update user share", function () {
-		return chakram.put(home + "/share/2")
+describe('Update user share', function () {
+	it('should update user share', function () {
+		return chakram.put(home + '/share/2')
 				.then((r) => {
 					expect(r.body.success).to.equal(true)
 				})
 	})
+})
+
+describe('Get validation mail', function () {
+
+	it('Should validate email', function() {
+		return chakram.get(home + 'user/valid/:token')
+				.then((r) => {
+					expect
+				})
+	})
+
+})
+
 })
