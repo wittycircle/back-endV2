914da7f5050ddf89c9b922975de38f59e879f922
Raphael Dantzer <rdantzer@student.42.fr>
2017-01-29 21:01:47 +0100

    auth test

---
+++ tests/schemas/auth.schema.js
@@ -0,0 +1,26 @@
+/**
+ * Created by rdantzer on 29/01/17.
+ */
+
+'use strict';
+
+/**
+ * Joi auth schema definition
+ */
+
+const joi = require('joi');
+
+const token_schema = joi.object().keys({
+    token: joi.string().length(64).required()
+});
+
+const user_schema = joi.object().keys({
+    id: joi.number().integer().required(),
+    profile_id: joi.number().integer().required(),
+    email: joi.string().email().required()
+});
+
+exports.auth_response_schema = joi.object().keys({
+    auth: token_schema,
+    user: user_schema
+});
