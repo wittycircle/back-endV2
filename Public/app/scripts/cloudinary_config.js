2a3e65bcc6c06b6a0f8100b2a48274fac762225f
Raphael Dantzer <rdantzer@student.42.fr>
2017-02-06 12:04:08 +0100

    Auf wiedersehen Public

--- Public/app/scripts/cloudinary_config.js
+++
@@ -1,5 +0,0 @@
-wittyCircleApp.config(['cloudinaryProvider', function (cloudinaryProvider) {
-  cloudinaryProvider
-      .set("cloud_name", "dqpkpmrgk")
-      .set("upload_preset", "z7rzegb5");
-}]);
