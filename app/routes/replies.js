98c9c66077656ae8bbea3914c9d49b07121c1da3
Raphael Sequoya <sequoya@wittycircle.com>
2017-02-10 22:42:22 +0100

    des bails

---
+++ app/routes/replies.js
@@ -0,0 +1,15 @@
+const express = require('express'),
+    router = express.Router(),
+    replies = require('../controllers/replies'),
+    {validate, validateParam, schemas} = require('../middlewares/validation'),
+    passport = require('passport');
+
+router.param('reply_id', validateParam(schemas.params.id));
+
+router.route('/replies/:reply_id')
+	.put(passport.authenticate('bearer'), validate(schemas.replies.reply), replies.updateReplyDiscussion)
+	.delete(passport.authenticate('bearer'), replies.removeReplyDiscussion)
+
+router.route('/replies/:reply_id/like')
+	.post(passport.authenticate('bearer'), validate(schemas.replies.reply))
+module.exports = router
---
+++ tests/replies.test.js
@@ -0,0 +1,50 @@
+const { db, TABLES } = require('../app/models/index');
+const p_empty = ['', null];
+
+module.exports = (storage, chakram) => {
+    const expect = chakram.expect,
+        route = storage.resource('replies/'),
+        rnd_string = Math.random().toString(36).slice(-10),
+        schemas = {
+            common: require('./schemas/common.schema'),
+            discussion: require('./schemas/discussion.schema'),
+            error: require('./schemas/error.schema')
+        };
+
+    describe('Should update reply from discussion [PUT /replies/reply_id]', function() {
+        let r, v, v2;
+        before('request', function() {
+            r = chakram.put(route + 263, {message: "Stupidty"});
+            v = chakram.put(route + 263);
+            v2 = chakram.put(route + storage.fakeId, {message: "lalala"});
+        });
+    
+        it('Should send success', function() {
+            return expect(r).to.joi(schemas.common.success).status(200);
+        });
+
+        it('Should match validation error', function() {
+        	expect(v).to.joi(schemas.error.validation_error_schema);
+        	expect(v2).to.joi(schemas.error.description);
+        	return chakram.wait();
+        });
+    });
+
+    describe('Should remove reply [comment] from discussion [DELETE /replies/reply_id]', function() {
+        let r, v;
+        before('request', function() {
+            r = chakram.delete(route + 263);
+            v = chakram.delete(route + )
+        });
+    
+        it('Should send success true', function() {
+            return expect(r).to.joi(schemas.common.success).status(200);
+        });
+
+        after('Cleanup db', function() {
+            db.raw('DELETE from project_discussion_replies where id > 260').return()
+            db.raw('alter table project_discussion_replies AUTO_INCREMENT = 263').return();
+        });
+    });
+
+};
