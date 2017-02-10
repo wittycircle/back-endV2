63a7a452cfde139498478bc0dfd23427f4e1c5f7
Raphael Sequoya <sequoya@wittycircle.com>
2017-02-10 17:03:04 +0100

    discussion schema

---
+++ app/middlewares/validation/schemas/discussion.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Joi = require('joi');
+const p_empty = ['', null]
+
+module.exports.discussion = Joi.object().keys({
+	title: Joi.string().trim().allow(p_empty).required(),
+	message: Joi.string().trim().allow(p_empty).required()
+});
+
+module.exports.reply = Joi.object().keys({
+	message: Joi.string().trim().allow(p_empty).required(),
+});
