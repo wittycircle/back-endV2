e95414555fecce928d82f7eca2a780748f5f6134
Raphael Dantzer <rdantzer@student.42.fr>
2017-02-19 09:51:07 +0100

    :construction: search implementation

--- app/models/search.js
+++ app/models/search.js
@@ -5,3 +5,40 @@
 'use strict';
 
 const {db, TABLES} = require('./index');
+
+/**
+ SELECT
+ p.id,
+ p.first_name,
+ p.last_name,
+ p.cover_picture,
+ p.profile_picture AS picture,
+ p.description,
+ p.city,
+ p.country,
+ p.state,
+ r.rank
+ FROM
+ profiles p
+ LEFT JOIN
+ rank_of_the_day r ON p.id = r.user_id;
+ */
+
+
+exports.searchProject = (query) => {
+};
+
+exports.searchProfile = (query) => {
+    return db(TABLES.USER_PROFILES)
+        .select([
+            'p.id',
+            'p.first_name',
+            'p.last_name',
+            'p.profile_picture AS picture',
+            'p.description',
+            'p.country',
+            'r.rank'])
+        .from(`${TABLES.USER_PROFILES} as p`)
+        .leftJoin(`${TABLES.RANKS} as r`, 'p.id', 'r.user_id')
+        .leftJoin(sub);
+};
