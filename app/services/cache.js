6bb42eae5decaba3dc22e4952eb1c7058649b4ef
Raphael Dantzer <rdantzer@student.42.fr>
2017-02-21 07:55:08 +0100

    :construction: pipeline redis commands for maximum efficiency

--- app/services/cache.js
+++ app/services/cache.js
@@ -14,15 +14,26 @@ const {db, TABLES} = require('../models'),
 
 
 const _bootstrap_user_follows = () => {
-    return db.raw('SELECT DISTINCT f.user_id, o.following, e.follower FROM user_followers f LEFT JOIN ' +
+    return db.raw('SELECT DISTINCT f.user_id AS owner, o.following, e.followers FROM user_followers f LEFT JOIN ' +
         '(SELECT u.user_id, COUNT(DISTINCT u.follow_user_id) ' +
         'AS following FROM user_followers u GROUP BY u.user_id) ' +
         'AS o ON f.user_id = o.user_id LEFT JOIN ' +
-        '(SELECT u.follow_user_id, COUNT(DISTINCT u.user_id) AS follower FROM user_followers u GROUP BY u.follow_user_id) ' +
+        '(SELECT u.follow_user_id, COUNT(DISTINCT u.user_id) AS followers FROM user_followers u GROUP BY u.follow_user_id) ' +
         'AS e ON f.user_id = e.follow_user_id ')
         .then(results => {
-            console.log(results);
             redis.set('user_cache_status', 'ok');
+            return results[0];
+
+        })
+        .then(result => {
+            return redis.pipeline(_.map(result, user => {
+                return [
+                    'hmset',
+                    _key('user_cache', user.owner),
+                    'followers', user.followers || 0,
+                    'following', user.following || 0
+                ]
+            })).exec();
         })
         .catch(err => {
             throw err
@@ -43,6 +54,12 @@ exports.init = () => {
                 redis.pipeline()
                     .hincrby(_key('user_cache', res.user_id), 'following', -1)
                     .hincrby(_key('user_cache', res.followed_id), 'followers', -1)
+                    .exec();
+                break;
+            case 'user_up_project' :
+                redis.pipeline()
+                    .hincrby(_key('project_cache', res.project_id), 'up', 1)
+                    .hincrby(_key('user_cache', res.user_id), 'followed_project')
                     .exec();
                 break;
             default:
