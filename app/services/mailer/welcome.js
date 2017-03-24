53f9f98d0070c13966f4ea46cc3cf5ea7e667801
Raphael Sequoya <sequoya@wittycircle.com>
2017-03-24 06:38:13 +0100

    fix welcome to match new  wm.substitutions:

--- app/services/mailer/welcome.js
+++ app/services/mailer/welcome.js
@@ -12,14 +12,13 @@ const welcome = (args) => {
 let	mail = new helper.Mail(),
 	pers = new helper.Personalization();
 
-return db.select('p.first_name as username')
+return db.first('p.first_name as username')
 		.from(TABLES.USERS + ' as u')
 		.join(TABLES.USER_PROFILES + ' as p', 'u.profile_id', 'p.id')
 		.where('u.email', args.email)
 	.then(username => {
-		username = _.map(username, 'username')
-			sub = { "-FNAME-" : username };
-			wm.subject(mail, pers, "Welcome to Witty !");
+			sub = { "-FNAME-" : username.username };
+			wm.subject(pers, "Welcome to Witty !");
 			wm.to(pers, /*args.email*/ 'sequoya@wittycircle.com');
 		  	wm.from(mail, 'quentin@wittycircle.com', "Quentin Verriere");
 			wm.substitutions(pers, sub)
