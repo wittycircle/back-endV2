a2f29d3203de733367ea7200d52919e2dd17ce37
Raphael Sequoya <sequoya@wittycircle.com>
2017-04-02 16:06:36 +0200

    reply discussion, added fields

--- app/models/discussions.js
+++ app/models/discussions.js
@@ -31,8 +31,11 @@ exports.getDiscussionReplies = (discussion_id) => {
 	let rep_like = (id) => db.select('user_id', 'creation_date')
 					.from(TABLES.PROJECT_REPLY_LIKES).where({'project_reply_id': id})
 
-	return	db.select( 'id', 'user_id', 'creation_date', 'message')
-			.from(TABLES.PROJECT_DISCUSSION_REPLIES).where('project_discussion_id', discussion_id)
+		return db.select([ 'rep.id', 'user_id', 'p.fullName', 'p.username', 'p.profile_picture',
+							 'creation_date', 'message'])
+			.from(TABLES.PROJECT_DISCUSSION_REPLIES + ' as rep')
+			.join(h.sub_profile, 'p.uid', 'rep.user_id')
+			.where('project_discussion_id', discussion_id)
 			.then(r => {
 				let x = []
 				r.forEach(el => {
