a8e24301b77ecdb3377ea9fcfbf62ad2d3cbe9d0
Raphael Sequoya <sequoya@wittycircle.com>
2017-04-05 18:58:25 +0200

    statistics

---
+++ app/controllers/statistics.js
@@ -0,0 +1,138 @@
+const statistics = require('../models/statistics'),
+    _ = require('lodash');
+
+
+
+// ------------------ NETWORKS ------------------
+exports.allProfiles = (req, res, next) => {
+	statistics.networkAllProfiles(req.params.name)
+		.then(r => {
+			if (typeof r === 'string') {
+				return next([r, 'Unknown network'])
+			}
+			else{
+				res.send(r)
+			}
+		})
+		.catch(err => next(err))
+};
+
+exports.allProjects = (req, res, next) => {
+	statistics.networkAllProjects(req.params.name)
+		.then(r => {
+			if (typeof r === 'string') {
+				return next([r, 'Unknown network'])
+			}
+			else{
+				res.send(r)
+			}
+		})
+		.catch(err => next(err))
+};
+
+//			*** Profiles ***
+
+exports.networkProfiles = (req, res, next) => {
+	statistics.networkProfiles(req.params.name)
+		.then(r => {
+			if (typeof r === 'string') {
+				return next([r, 'Unknown network'])
+			}
+			else{
+				res.send({count: r.length, profiles: r})
+			}
+		})
+		.catch(err => next(err))
+};
+
+exports.networkProfileSkills = (req, res, next) => {
+	statistics.networkProfileSkills(req.params.name)
+		.then(r => {
+			if (typeof r === 'string') {
+				return next([r, 'Unknown network'])
+			}
+			else{
+				res.send({skills: r})
+			}
+		})
+		.catch(err => next(err))
+};
+
+exports.networkLocation = (req, res, next) => {
+	statistics.networkLocation(req.params.name)
+		.then(r => {
+			if (typeof r === 'string') {
+				return next([r, 'Unknown network'])
+			}
+			else{
+				res.send({location: r})
+			}
+		})
+		.catch(err => next(err))
+};
+exports.networkInterests = (req, res, next) => {
+	statistics.networkInterests(req.params.name)	
+		.then(r => {
+			if (typeof r === 'string') {
+				return next([r, 'Unknown network'])
+			}
+			else{
+				res.send({interests: r})
+			}
+		})
+		.catch(err => next(err))
+};
+
+// ------------------  ------------------
+//			*** Projects ***
+exports.networkProjects = (req, res, next) => {
+	statistics.networkProjects(req.params.name)
+		.then(r => {
+			if (typeof r === 'string') {
+				return next([r, 'Unknown network'])
+			}
+			else{
+				res.send({projects: r})
+			}
+		})
+		.catch(err => next(err))
+};
+
+exports.networkProjectAbout = (req, res, next) => {
+	statistics.networkProjectAbout(req.params.name)
+		.then(r => {
+			if (typeof r === 'string') {
+				return next([r, 'Unknown network'])
+			}
+			else{
+				res.send({About: r})
+			}
+		})
+		.catch(err => next(err))
+};
+
+exports.networkProjectNeeds = (req, res, next) => {
+	statistics.networkProjectNeeds(req.params.name)
+		.then(r => {
+			if (typeof r === 'string') {
+				return next([r, 'Unknown network'])
+			}
+			else{
+				res.send({needs: r})
+			}
+		})
+		.catch(err => next(err))
+};
+
+exports.networkProjectFollow = (req, res, next) => {
+	statistics.networkProjectFollow(req.params.name)
+		.then(r => {
+			if (typeof r === 'string') {
+				return next([r, 'Unknown network'])
+			}
+			else{
+				res.send({most_upvoted: r})
+			}
+		})
+		.catch(err => next(err))
+};
---
+++ app/models/statistics.js
@@ -0,0 +1,157 @@
+const {db, TABLES} = require('./index'),
+        _ = require('lodash'),
+        h = require('./helper');
+
+exports.networkAllProfiles = (name) => {
+	return h.exist(TABLES.USER_PROFILES, name, 'network').then(r => {
+		if (!r.length)
+			return "error"
+		return Promise.all([np(name), nps(name), nl(name), ni(name)])
+			.then(r => {
+				return {
+					profiles: r[0], 
+					skills: r[1], 
+					location: r[2], 
+					interests: r[3]
+				}
+			  });
+	})
+};
+
+exports.networkAllProjects = (name) => {
+	return h.exist(TABLES.USER_PROFILES, name, 'network').then(r => {
+		if (!r.length)
+			return "error"
+		return Promise.all([npr(name), npra(name), nprn(name), nprf(name)])
+			.then(r => {
+				return {
+					projects: r[0], 
+					about: r[1], 
+					needs: r[2], 
+					follow: r[3]
+				}
+			  });
+	})
+};
+
+//			*** Profiles ***
+
+
+np = exports.networkProfiles = (name) => {
+	return h.exist(TABLES.USER_PROFILES, name, 'network').then(r => {
+		if (!r.length)
+			return "error"
+		return db.select(['u.username','p.profile_picture', 'p.id',
+						db.raw('CONCAT (p.first_name, " ", p.last_name) as fullName')])
+				.from(TABLES.USERS + ' as u')
+				.join(TABLES.USER_PROFILES + ' as p', 'p.id', 'u.profile_id')
+				.whereRaw(`p.network LIKE "%${name}%"`)
+	})
+};
+
+nps = exports.networkProfileSkills = (name) => {
+	return h.exist(TABLES.USER_PROFILES, name, 'network').then(r => {
+		if (!r.length)
+			return "error"
+		return db.distinct('s.name', 's.category')
+				.countDistinct('p.uid as count')
+				.from(h.sub_profile)
+				.join(TABLES.USER_SKILLS + ' as us', 'us.user_id', 'p.uid')
+				.join(TABLES.SKILLS + ' as s', 'us.skill_id', 's.id')
+				.whereRaw(`p.network LIKE "%${name}%"`)
+				.groupBy('s.name')
+				.orderByRaw('count DESC')
+	})
+};
+
+nl = exports.networkLocation = (name) => {
+	return h.exist(TABLES.USER_PROFILES, name, 'network').then(r => {
+		if (!r.length)
+			return "error"
+		return db.distinct('p.country')
+				.countDistinct('p.id as count')
+				.from(TABLES.USER_PROFILES + ' as p')
+				.whereRaw(`p.network LIKE "%${name}%"`)
+				.whereRaw('p.country IS NOT NULL AND p.country <> ""')
+				.orderByRaw('count DESC')
+				.groupBy('p.country')
+	})
+};
+
+ni = exports.networkInterests = (name) => {
+		return h.exist(TABLES.USER_PROFILES, name, 'network').then(r => {
+		if (!r.length)
+			return "error"
+		return db.distinct('c.name')
+			.from(h.sub_profile)
+			.countDistinct('p.id as count')
+			.join(TABLES.PROJECT_LIKES + ' as pl', 'p.uid', 'pl.user_id')
+			.join(TABLES.PROJECTS + ' as pr', 'pr.id', 'pl.project_id')
+			.join(TABLES.CATEGORIES + ' as c', 'pr.category_id', 'c.id')
+			.whereRaw(`p.network LIKE "%${name}%"`)
+			.orderByRaw('count DESC')
+			.groupBy('c.name')
+
+	})
+};
+
+//			*** Projects ***
+npr = exports.networkProjects = (name) => {
+	return h.exist(TABLES.USER_PROFILES, name, 'network').then(r => {
+		if (!r.length)
+			return "error"
+		return db(TABLES.PROJECTS + ' as pr')
+				.select('pr.id')
+				.count('pr.id as count')
+				.join(h.sub_profile, 'p.uid', 'pr.user_id')
+				.whereRaw(`p.network LIKE "%${name}%"`)
+	})
+};
+
+npra = exports.networkProjectAbout = (name) => {
+	return h.exist(TABLES.USER_PROFILES, name, 'network').then(r => {
+		if (!r.length)
+			return "error"
+		return db.distinct('c.name as category')
+				.countDistinct('p.id as count')
+				.from(TABLES.PROJECTS + ' as pr')
+				.join(h.sub_profile, 'pr.user_id', 'p.uid')
+				.join(TABLES.CATEGORIES + ' as c', 'c.id', 'pr.category_id')
+				.whereRaw(`p.network LIKE "%${name}%"`)
+				.groupBy('c.name')
+				.orderByRaw('count DESC')
+	})
+};
+
+nprn = exports.networkProjectNeeds = (name) => {
+		return h.exist(TABLES.USER_PROFILES, name, 'network').then(r => {
+		if (!r.length)
+			return "error"
+		return db.distinct('ot.tag')
+			.countDistinct('p.id as count')
+			.from(TABLES.PROJECTS + ' as pr')
+			.join(h.sub_profile, 'pr.user_id', 'p.uid')
+			.join(TABLES.PROJECT_OPENINGS + ' as o', 'o.project_id', 'pr.id')
+			.join(TABLES.OPENING_TAGS + ' as ot', 'ot.opening_id', 'o.id')
+			.whereRaw(`p.network LIKE "%${name}%"`)
+			.groupBy('ot.tag')
+			.orderByRaw('count DESC')
+	})
+};
+
+nprf = exports.networkProjectFollow = (name) => {
+		return h.exist(TABLES.USER_PROFILES, name, 'network').then(r => {
+		if (!r.length)
+			return "error"
+		return db.distinct('pr.title')
+				.countDistinct('l.user_id as count')
+				.from(TABLES.PROJECTS + ' as pr')
+				.join(h.sub_profile, 'pr.user_id', 'p.uid')
+				.join(TABLES.PROJECT_LIKES + ' as l', 'l.project_id', 'pr.id')
+				.whereRaw(`p.network LIKE "%${name}%"`)
+				.groupBy('pr.id')
+				.orderByRaw('count DESC')
+		})
+};
+
+
