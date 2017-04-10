cb608f899a1fabe0248714d5c616d51e46fcc3c0
Raphael Sequoya <sequoya@wittycircle.com>
2017-04-10 10:18:22 +0200

    renaming stuff

--- app/models/search.js
+++ app/models/search.js
@@ -102,7 +102,7 @@ exports.cardProject = (selector) => {
     END as location 
 `);
 
-    const pr_array = ['pr.id', 'pr.title', 'pr.description', 'pr.picture_card', 'pr.status', 'pr.public_id',
+    const pr_array = ['pr.id', 'pr.title', 'pr.description', 'pr.picture_card as picture', 'pr.status', 'pr.public_id',
         'c.id as category_id', 'c.name as category_name', 'pr.network as project_network',
      'p.network', 'p.profile_picture', 'p.uid as user_id', db.raw('CONCAT (p.first_name, " ", p.last_name) as username'),
      project_location,
