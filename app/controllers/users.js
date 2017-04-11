442a5d17b6cb0128d0a4009b7f62defdde20a5a1
Raphael Sequoya <sequoya@wittycircle.com>
2017-04-12 01:19:58 +0200

    picture card to picture

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -24,23 +24,23 @@ exports.createUser = (req, res, next) => {
         if (exist.length) {
             res.status(400).send({success: false, msg: 'Email is already taken', exist: exist});
         } else {
-            checkUsername(req.body.first_name, req.body.last_name, req.body.password)
-                .then(({firstName, lastName, username, password}) => {
-                    user.createProfile(firstName, lastName)
-                        .then(([profileId]) => {
-                            user.createUser(profileId, req.body.email, username, password)
-                                .then(([userId]) => {
-                                    user.updateProfile({username: username}, profileId)
-                                        .then(user.updateInvitation)
-                                        .then(res.send({user: {id: userId, profile_id: profileId}}))
-                                        .catch((e) => {
-                                            console.error(e)
-                                        })
-                                })
-                        })
-                }).catch(err => next(err))
-        }
-    })
+            checkUsername(req.body.first_name, req.body.last_name, req.body.password) 
+            .then(({firstName, lastName, username, password}) => {
+                user.createProfile(firstName, lastName) 
+                .then(([profileId]) => {
+                    user.createUser(profileId, req.body.email, username, password) 
+                    .then(([userId]) => {
+                        user.updateProfile({username: username}, profileId) 
+                        .then(user.updateInvitation) 
+                        .then(res.send({user: {id: userId, profile_id: profileId}})) 
+                        .catch((e) => {
+                            console.error(e) 
+                        }) 
+                    }) 
+                }) 
+            }).catch(err => next(err)) 
+        } 
+    }) 
 };
 // ------------------ SKILLS ------------------
 exports.getUserSkills = (req, res, next) => {
