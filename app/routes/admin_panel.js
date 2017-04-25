4807875fb476f2e4f93d1ffd6d95555ba64f2472
Raphael Sequoya <sequoya@wittycircle.com>
2017-04-25 17:00:45 +0200

    Admin panel

---
+++ app/routes/admin_panel.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express'),
+    router = express.Router(),
+    panel = require('../controllers/admin_panel'),
+    search = require('../controllers/search'),
+    {validate, validateParam, schemas} = require('../middlewares/validation'),
+    {auth, AUTH} = require('../services/auth');
+
+router.route('/admin_panel/projects')
+	.get(auth(AUTH.PRIVATE), panel.projectsInvite)
+	.post(auth(AUTH.PRIVATE), panel.inviteProjects)
+
+router.param('token', validateParam(schemas.params.name));
+
+router.route('/admin_panel/projects/:token')
+	.post(auth(AUTH.PRIVATE), panel.updateCreator)
+
+module.exports = router;
