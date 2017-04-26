e4dfb069ac46facd91df6236989b85b1e37c8b14
Raphael Dantzer <rdantzer@student.42.fr>
2017-04-26 07:21:47 +0200

    magie

--- app/services/mailer/index.js
+++ app/services/mailer/index.js
@@ -1,46 +1,43 @@
 /*
-[line number [approximate due to change]]
--	ask_project: controllers/projects  [106]
--	upvote_project: controllers/projects [217]
-	invite_user: controllers/project [242],  -> UNDEFINED
-	invite_team: UNDEFINED,
-	new_message: UNDEFINED,
--	new_project: controllers/projects [35],
--	reply_project: controllers/discussion [56],
--	reset_password: controllers/account [74],
-	suggestion_profile: ,
-	suggestion_project: ,
-	uc_invitation: UNDEFINED,
--	user_follow: controllers/profiles [71],
--	validate_account: controllers/accounts [45],
-	validation-network: FOR LATER,
-	verification_network: controllers/network [52] (Way unsure about this one),
-	profile_views:,
--	welcome : controllers/account [59]
-*/
+ [line number [approximate due to change]]
+ -	ask_project: controllers/projects  [106]
+ -	upvote_project: controllers/projects [217]
+ invite_user: controllers/project [242],  -> UNDEFINED
+ invite_team: UNDEFINED,
+ new_message: UNDEFINED,
+ -	new_project: controllers/projects [35],
+ -	reply_project: controllers/discussion [56],
+ -	reset_password: controllers/account [74],
+ suggestion_profile: ,
+ suggestion_project: ,
+ uc_invitation: UNDEFINED,
+ -	user_follow: controllers/profiles [71],
+ -	validate_account: controllers/accounts [45],
+ validation-network: FOR LATER,
+ verification_network: controllers/network [52] (Way unsure about this one),
+ profile_views:,
+ -	welcome : controllers/account [59]
+ */
 
 const filt = e => e !== 'templates'
-				&& e !== 'index.js'
-				&& e !== 'wittymail.js' 
-				&& e !== 'example_kitchen.js';
+&& e !== 'index.js'
+&& e !== 'wittymail.js'
+&& e !== 'example_kitchen.js';
 
 const files = require('fs').readdirSync('./app/services/mailer')
-				.filter(filt);
+    .filter(filt);
 
 let mails = {};
-// ------------------ PROD ENVIRONMENT ------------------
-files.forEach(e => {
-	let n = e.split('.')[0]
-	mails[n]  = (args) => require(`./${n}`)(args)
-});
-
-// ------------------ DEV ENVIRONMENT ------------------
-
-
-// files.forEach(e => {
-// 	let n = e.split('.')[0]
-// 	mails[n]  = (args) => console.log(`no [ ${n} ] mail, dev environment`)
-// });
+if (process.env.NODE_ENV != 'development')
+    files.forEach(e => {
+        let n = e.split('.')[0]
+        mails[n] = (args) => require(`./${n}`)(args)
+    });
+else
+//     files.forEach(e => {
+//         let n = e.split('.')[0]
+//         mails[n] = (args) => console.log(`no [ ${n} ] mail, dev environment`)
+//     });
 
 
 module.exports = mails
