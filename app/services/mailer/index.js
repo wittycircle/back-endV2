318bdbb8cf17b402560875eb0a180f194555c943
Raphael Sequoya <sequoya@wittycircle.com>
2017-04-28 05:34:00 +0200

    removed readdir and put files by hand

--- app/services/mailer/index.js
+++ app/services/mailer/index.js
@@ -19,29 +19,43 @@
 S -	welcome : controllers/account [59]
  */
 
-const filt = e => e !== 'templates'
-&& e !== 'index.js'
-&& e !== 'wittymail.js'
-&& e !== 'example_kitchen.js';
+// const filt = e => e !== 'templates'
+// && e !== 'index.js'
+// && e !== 'wittymail.js'
+// && e !== 'example_kitchen.js';
 
-const files = require('fs').readdirSync('../app/services/mailer')
-    .filter(filt);
+// const files = require('fs').readdirSync('../app/services/mailer')
+//     .filter(filt);
 
+
+const files = [ 'ask_project',
+  'invite_team',
+  'invite_user',
+  'new_message',
+  'new_project',
+  'profile_views',
+  'reply_project',
+  'reset_password',
+  'suggestion_profile',
+  'uc_invitation',
+  'upvote_project',
+  'user_follow',
+  'validate_account',
+  'verification_network',
+  'welcome' ];
 
 let mails = {}
 
 if (process.env.NODE_ENV === "development") 
 {
 	files.forEach(e => {
-		let n = e.split('.')[0] 
-		mails[n]  = (args) => console.log(`no [ ${n} ] mail, dev environment`) 
+		mails[e]  = (args) => console.log(`no [ ${e} ] mail, dev environment`) 
 	}); 
 }
 // ------------------ PROD ENVIRONMENT ------------------
 else {
 	files.forEach(e => {
-		let n = e.split('.')[0] 
-		mails[n]  = (args) => require(`./${n}`)(args) 
+		mails[e]  = (args) => require(`./${e}`)(args) 
 	});
  }
 
