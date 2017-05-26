1af74f460d184799f9d83740f9b7ac83b3747e8b
Raphael Sequoya <sequoya@wittycircle.com>
2017-05-26 19:46:08 +0200

    Joi joi

--- app/middlewares/validation/schemas/project.js
+++ app/middlewares/validation/schemas/project.js
@@ -5,56 +5,54 @@
 'use strict';
 
 const Joi = require('joi'),
-	common = require('./common'),
- 	p_empty = ['', null],
-	p_status = ['any', 'tips', 'teammate', 'mentor'];
+  common = require('./common'),
+  p_empty = ['', null],
+  p_status = ['any', 'tips', 'teammate', 'mentor'];
 
 const discussion = Joi.object().keys({
-	title: Joi.string().trim().allow(p_empty),
-	message: Joi.string().trim().allow(p_empty).required()
+  title: Joi.string().trim().allow(p_empty),
+  message: Joi.string().trim().allow(p_empty).required()
 });
 
 const opening = Joi.object().keys({
-	skill: Joi.string().trim().allow(p_empty),
-	status: Joi.string().trim().allow(p_status).required(),
-	description:Joi.string().trim().allow(p_empty),
-	tags: Joi.string().trim().allow(p_empty),
+  skill: Joi.string().trim().allow(p_empty),
+  status: Joi.string().trim().allow(p_status).required(),
+  description: Joi.string().trim().allow(p_empty),
+  tags: Joi.string().trim().allow(p_empty)
 });
 
 module.exports.opening = opening;
 module.exports.discussion = discussion;
 
 module.exports.creation = Joi.object().keys({
-	title: Joi.string().trim().allow(p_empty).required(),
-	category: Joi.number().integer().required(),
-	location: common.location.required(),
-    status: Joi.string().trim().required(),
-    picture: Joi.string().trim().allow(p_empty)/*.required()*/,
-    video: Joi.string().trim().allow(p_empty)/*.required()*/,
-    description: Joi.string().trim().allow(p_empty)/*.required()*/,
-    about: Joi.string().trim().allow(p_empty)/*.required()*/,
-    network: Joi.string().trim().allow(p_empty)/*.required()*/,
-    project_visibility: Joi.boolean()/*.required()*/,
-    members: Joi.array().items(Joi.number().integer()/*.required()*/),
-	openings: Joi.array().items(opening),
-	discussions: Joi.array().items(discussion)
+  title: Joi.string().trim().allow(p_empty).required(),
+  category: Joi.number().integer().required(),
+  location: common.location.required(),
+  status: Joi.string().trim().required(),
+  picture: Joi.string().trim().allow(p_empty) /*.required()*/,
+  video: Joi.string().trim().allow(p_empty) /*.required()*/,
+  description: Joi.string().trim().allow(p_empty) /*.required()*/,
+  about: Joi.string().trim().allow(p_empty) /*.required()*/,
+  network: Joi.string().trim().allow(p_empty) /*.required()*/,
+  project_visibility: Joi.boolean() /*.required()*/,
+  members: Joi.array().items(Joi.number().integer() /*.required()*/),
+  openings: Joi.array().items(opening),
+  discussions: Joi.array().items(discussion)
 });
 
 module.exports.update = Joi.object().keys({
-	title: Joi.string().trim().allow(p_empty),
-	category: Joi.number().integer(),
-    city: Joi.string(),
-    country: Joi.string(),
-    state: Joi.string(),
-	picture: Joi.string().trim().allow(p_empty),
-    picture_card: Joi.string().trim().allow(p_empty),
-	video: Joi.string().trim().allow(p_empty),
-	description: Joi.string().trim().allow(p_empty),
-	about: Joi.string().trim().allow(p_empty),
-	network: Joi.string().trim().allow(p_empty),
-	link: Joi.string().trim().allow(p_empty),
-	app: Joi.string().trim().allow(p_empty),
-	logo: Joi.string().trim().allow(p_empty),
-	public: Joi.boolean(),
-    project_visibility: Joi.boolean(),
+  title: Joi.string().trim().allow(p_empty),
+  location: common.location.required(),
+  category: Joi.number().integer(),
+  picture: Joi.string().trim().allow(p_empty),
+  picture_card: Joi.string().trim().allow(p_empty),
+  video: Joi.string().trim().allow(p_empty),
+  description: Joi.string().trim().allow(p_empty),
+  about: Joi.string().trim().allow(p_empty),
+  network: Joi.string().trim().allow(p_empty),
+  link: Joi.string().trim().allow(p_empty),
+  app: Joi.string().trim().allow(p_empty),
+  logo: Joi.string().trim().allow(p_empty),
+  public: Joi.boolean(),
+  project_visibility: Joi.boolean()
 });
