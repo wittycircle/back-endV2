310380176fcdaebc5eb93f571e2efc79dc455a3d
Raphael Sequoya <sequoya@wittycircle.com>
2017-06-05 19:37:34 +0200

    google invite

--- app/controllers/social.js
+++ app/controllers/social.js
@@ -6,17 +6,19 @@ const social = require('../services/social');
 const mailer = require('../services/mailer');
 
 exports.updateProfileFromLinkedin = (req, res, next) => {
-    const { profile } = req.body;
-    const user = req.user;
+  const { profile } = req.body;
+  const user = req.user;
 
-    social.getLinkedinProfileInfo(profile)
-        .then(results => res.send(results));
+  social.getLinkedinProfileInfo(profile).then(results => res.send(results));
 };
 
 exports.InviteFriendsFromGoogle = (req, res, next) => {
-    const { token } = req.body;
+  const { token } = req.body;
 
-    social.gmailContactsCampaign(token)
-        .then(mailList => mailer.invite_user({uid: req.user, mailList, category: 'gmail'}))
-        .then(() => res.send({success: true}))
+  social
+    .gmailContactsCampaign(token)
+    .then(mailList =>
+      mailer.invite_user({ uid: req.user, mailList, category: 'gmail' })
+    )
+    .then(() => res.send({ success: true }));
 };
