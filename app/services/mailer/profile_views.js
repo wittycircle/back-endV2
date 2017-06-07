e3698975c8a5881f9be8e2508119c587c1ba1839
Raphael Sequoya <sequoya@wittycircle.com>
2017-06-07 17:33:11 +0200

    profile views

--- app/services/mailer/profile_views.js
+++ app/services/mailer/profile_views.js
@@ -7,7 +7,7 @@ const _ = require('lodash');
 const fillSub = (d, sub, i) => {
   sub[`*|FFNAME${i}|*`] = d.first_name;
   sub[`*|FLNAME${i}|*`] = d.last_name;
-  sub[`*|FIMG${i}|*`] = d.profile_picture;
+  sub[`*|FIMG${i}|*`] = d.picture;
   sub[`*|FLOC${i}|*`] = d.location;
   sub[`*|FDESC${i}|*`] = wm.truncate(d.description);
   sub[`*|FURL${i}|*`] = wm.url(d.username);
@@ -29,16 +29,19 @@ const send_mail = (data, bail) => {
     let nview = notif.length;
     let sub = {
       '*|FNAME|*': e.first_name,
+      '*|RANK|*': e.rank,
       '*|NVIEW|*': nview.toString(),
       '*|EMAIL|*': e.email
     };
     bail[i].forEach((b, j) => fillSub(b, sub, j + 1));
+    // console.log('\n-------------------------------------------------\n');
+    // console.log(sub);
     wm.subject(pers, subject);
     wm.to(pers, e.email);
     wm.substitutions(pers, sub);
     mail.addPersonalization(pers);
   }); //foreach
-  wm.send(mail);
+  // wm.send(mail);
   return null;
 };
 
@@ -47,30 +50,36 @@ const profile_views = args => {
     .distinct(
       'v.viewed',
       'u.email',
+      'r.rank',
       'p.first_name',
       'p.description',
-      db.raw('GROUP_CONCAT(v.user_id) as notif')
+      db.raw('GROUP_CONCAT(distinct v.user_id) as notif')
     )
-    .count('v.user_id as vcount')
+    .countDistinct('v.user_id as vcount')
     .from('views as v')
     .join(TABLES.USERS + ' as u', 'v.viewed', 'u.id')
+    .join(TABLES.RANK + ' as r', 'r.user_id', 'u.id')
     .join(TABLES.PROFILES + ' as p', 'p.user_id', 'u.id')
     .join(wm.notif('profile_view'), 'n.user_id', 'v.viewed')
-    // .having('vcount', '>=', 5)
-    // .andWhere('mail_sent', 0)
+    .having('vcount', '>=', 5)
+    .andWhere('mail_sent', 0)
     .groupBy('v.viewed');
-  let reqAll = notif =>
+
+  const reqAll = notif =>
     db
       .distinct(h.p_uarray.concat([h.format_location]))
+      .distinct(db.raw('CONCAT(loc.city, ", ", loc.country) as location'))
       .from(TABLES.PROFILES + ' as p')
       .join(TABLES.LOCATION + ' as loc', 'loc.id', 'p.loc_id')
       .join(TABLES.USERS + ' as u', 'u.id', 'p.user_id')
       .whereIn('u.id', notif);
 
-  let setToOne = ids => db('views').update('mail_sent', 1).whereIn('viewed', ids);
+  const setToOne = ids =>
+    db('views').update('mail_sent', 1).whereIn('viewed', ids);
 
   return request.then(array => {
     if (!array.length) {
+      console.log(request.toString());
       console.log('empty');
       return null;
     }
@@ -81,7 +90,9 @@ const profile_views = args => {
       x.push(reqAll(notif).then(x => x));
     }); //foreach
     x.push(setToOne(ids).return());
-    return Promise.all(x).then(bail => send_mail(array, bail));
+    return Promise.all(x).then(bail => {
+      send_mail(array, bail);
+    });
   });
 }; //exports
 
