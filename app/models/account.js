9d783ac96ef10a97b0ed346447e51b908d6deea6
Raphael Sequoya <sequoya@wittycircle.com>
2017-06-16 00:26:07 +0200

    fix

--- app/models/account.js
+++ app/models/account.js
@@ -55,7 +55,7 @@ const social_helper = {
         user: {
           email: data.emails[0].value,
           username: _.replace(data.displayName, ' ', '.'),
-          invite_link: `${data.first_name.replace(/ /g, '')}${data.last_name.replace(/ /g, '')}_W${+Math.floor(Math.random() * 10000 + 1)}`
+          invite_link: `${data.name.givenName.replace(/ /g, '')}${data.name.familyName.replace(/ /g, '')}_W${+Math.floor(Math.random() * 10000 + 1)}`
         }
       };
     }),
