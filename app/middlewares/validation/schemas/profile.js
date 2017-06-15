21104fe672e0a49e520420ddf674403ab631a5e1
Raphael Dantzer <rdantzer@student.42.fr>
2017-06-15 22:09:43 +0200

    :wrench: description as optional

--- app/middlewares/validation/schemas/profile.js
+++ app/middlewares/validation/schemas/profile.js
@@ -10,7 +10,7 @@ const profile_update = Joi.object().keys({
   first_name: Joi.string().alphanum().trim().min(1).max(64).optional(),
   last_name: Joi.string().alphanum().trim().min(1).max(64).optional(),
   about: Joi.string().max(10000).optional(),
-  description: Joi.string().optional(),
+  description: Joi.optional(),
   picture: Joi.string().optional(),
   cover_picture: Joi.string().optional(),
   genre: Joi.string().optional(),
