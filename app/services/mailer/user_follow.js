e5a5cb3d774773a8d05b01bf2dd3dbb1b098774d
Raphael Sequoya <sequoya@wittycircle.com>
2017-06-16 21:57:47 +0200

    follow mail

--- app/services/mailer/user_follow.js
+++ app/services/mailer/user_follow.js
@@ -41,6 +41,7 @@ const send_mail = (follower, followed) => {
 const user_follow = args => {
   const follower = h
     .spe_profile({ 'u.id': args.follower })
+    .join(TABLES.LOCATION + 'as loc', 'loc.id', 'p.loc_id')
     .select(h.format_location);
 
   // console.log(h.format_location)
