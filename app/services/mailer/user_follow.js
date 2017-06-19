6ec7dca086b993c936993b3ad841094afa28a6ac
Raphael Sequoya <sequoya@wittycircle.com>
2017-06-19 16:30:26 +0200

    fix location

--- app/services/mailer/user_follow.js
+++ app/services/mailer/user_follow.js
@@ -42,7 +42,11 @@ const user_follow = args => {
   const follower = h
     .spe_profile({ 'u.id': args.follower })
     .join(TABLES.LOCATION + ' as loc', 'loc.id', 'p.loc_id')
-    .select('p.*', h.format_location);
+    .select(
+      'p.*',
+      h.format_location,
+      db.raw('CONCAT(loc.city, ", ", loc.country) as location')
+    );
 
   // console.log(h.format_location)
   // console.log(follower.toString())
