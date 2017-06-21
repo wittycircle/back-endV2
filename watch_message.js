ed28c85d4ca37877ccf271fc83c98b44c19e4085
Raphael Sequoya <sequoya@wittycircle.com>
2017-06-21 19:46:16 +0200

    watch message, update ranking fix

--- watch_message.js
+++ watch_message.js
@@ -2,18 +2,25 @@ const { db, TABLES } = require('./app/models'),
   mailer = require('./app/services/mailer');
 
 //Name of file not appropriate, more like setTimeout stuff misc
+let QUARTER_HOUR = 1000 * 90;
 let HALF_HOUR = 3600 * 500;
 let ONE_DAY = 3600 * 24 * 1000;
 let ONE_WEEK = ONE_DAY * 7;
 
 let updateRanking = () => {
-  return db.raw(`delete from ranks;
-	alter table ranks AUTO_INCREMENT = 1;
-	insert into ranks (user_id, rank) select user_id, @rank:=@rank+1 as rank from (select @rank:=0) r, rank_points s order by points desc;`);
+  console.log('Update called');
+  return db
+    .raw(`delete from ranks`)
+    .then(() => db.raw('alter table ranks AUTO_INCREMENT = 1'))
+    .then(() =>
+      db.raw(`insert into ranks
+        (user_id, rank) select user_id, @rank:=@rank+1 as rank
+        from (select @rank:=0) r, rank_points s order by points desc`)
+    );
 };
 
 module.exports = () => {
   setInterval(mailer.new_message, HALF_HOUR);
   setInterval(mailer.profile_views, ONE_WEEK);
-  setInterval(updateRanking, HALF_HOUR);
+  setInterval(updateRanking, QUARTER_HOUR);
 };
