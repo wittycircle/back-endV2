83d6dbf21a489c13a682f78cd57754498dbabd76
Raphael Sequoya <sequoya@wittycircle.com>
2017-07-28 01:00:24 +0200

    bail skill

---
+++ bail_skill/new_category_skill.js
@@ -0,0 +1,82 @@
+module.exports = [
+  {
+    'category ID': 1,
+    subcategory: 'Business',
+    'number of skills': 243
+  },
+  {
+    'category ID': 2,
+    subcategory: 'Creativity',
+    'number of skills': 63
+  },
+  {
+    'category ID': 3,
+    subcategory: 'Databases',
+    'number of skills': 37
+  },
+  {
+    'category ID': 4,
+    subcategory: 'Design',
+    'number of skills': 64
+  },
+  {
+    'category ID': 5,
+    subcategory: 'Manufacturing and Engineering',
+    'number of skills': 83
+  },
+  {
+    'category ID': 6,
+    subcategory: 'Game Development',
+    'number of skills': 18
+  },
+  {
+    'category ID': 7,
+    subcategory: 'IT',
+    'number of skills': 93
+  },
+  {
+    'category ID': 8,
+    subcategory: 'Languages',
+    'number of skills': 28
+  },
+  {
+    'category ID': 9,
+    subcategory: 'Sales & Marketing',
+    'number of skills': 109
+  },
+  {
+    'category ID': 10,
+    subcategory: 'Mobile Development',
+    'number of skills': 31
+  },
+  {
+    'category ID': 11,
+    subcategory: 'Software development',
+    'number of skills': 63
+  },
+  {
+    'category ID': 12,
+    subcategory: 'Web Design',
+    'number of skills': 33
+  },
+  {
+    'category ID': 13,
+    subcategory: 'Web Development',
+    'number of skills': 81
+  },
+  {
+    'category ID': 14,
+    subcategory: 'Writing',
+    'number of skills': 44
+  },
+  {
+    'category ID': 15,
+    subcategory: 'Tools',
+    'number of skills': 121
+  },
+  {
+    'category ID': 16,
+    subcategory: 'Random',
+    'number of skills': 45
+  }
+];
