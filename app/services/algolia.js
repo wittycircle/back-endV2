5f11e964abf985d25c25d8843cf0f31d30a46efa
root <root@ip-172-31-17-115.eu-west-1.compute.internal>
2017-08-17 20:09:51 +0000

    update algolia database

--- app/services/algolia.js
+++ app/services/algolia.js
@@ -18,32 +18,31 @@ const init = (module.exports.initPeopleAndProjectIndex = () => {
 
 	let storage = {};
 
-	Promise.all([
-		db(h.spe_profile({ 'u.fake': 0 }).select('u.fake'))
-			.join(TABLES.LOCATION + ' as loc', 'loc.id', 'p.loc_id')
-			.select('p.*', h.format_location)
-			.then(profiles => {
-				storage.profiles = profiles;
-				client.deleteIndex('Users', err => {
-					console.log(profiles);
-					people.addObjects(profiles.filter(p => p.fullName && p.picture));
-				});
-			}),
-		db(TABLES.PROJECTS + ' as p')
-			.select('*')
-			.innerJoin('categories', 'categories.id', 'p.category_id')
-			.where('p.project_visibility', 1)
-			.then(projects => {
-				storage.projects = projects;
-				client.deleteIndex('Projects', err => {
-					project.addObjects(projects);
-				});
-			})
+    Promise.all([
+	db(h.spe_profile({ 'u.fake': 0 }).select('u.fake'))
+	.join(TABLES.LOCATION + ' as loc', 'loc.id', 'p.loc_id')
+	.select('p.*', h.format_location)
+	.then(profiles => {
+	    storage.profiles = profiles;
+	    client.deleteIndex('Users', err => {
+		people.addObjects(profiles.filter(p => p.fullName && p.picture));
+		});
+	    }),
+	db(TABLES.PROJECTS + ' as p')
+	.select('*')
+	.innerJoin('categories', 'categories.id', 'p.category_id')
+	.where('p.project_visibility', 1)
+	.then(projects => {
+	    storage.projects = projects;
+	    client.deleteIndex('Projects', err => {
+		project.addObjects(projects);
+		});
+	    })
 	]).then(() =>
 		client.deleteIndex('PAndP', err =>
-			pandp.addObjects([...storage.profiles, ...storage.projects])
-		)
-	);
+				   pandp.addObjects([...storage.profiles, ...storage.projects])
+				   )
+		);
 });
 init();
 //setInterval(init, 3600 * 24);
