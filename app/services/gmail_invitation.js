71ddc1de5ce1c30fef3bd01d0cb042712c244611
jayho1 <lionfiercely_force@hotmail.fr>
2017-08-21 18:54:55 -0700

    update

---
+++ app/services/gmail_invitation.js
@@ -0,0 +1,26 @@
+const { db, TABLES } = require('../models/index'),
+	mailer 			= require('./mailer'),
+	check 			= require('../models/invitation');
+
+const sendInvitationToGmailContacts = () => {
+	db('gmail_auth_contacts').select('user_id').groupBy('user_id')
+	.then(r => {
+		function recursive(index) {
+			if (index < r.length) {
+				db('gmail_auth_contacts')
+				.select('mail_to')
+				.where('user_id', r[index].user_id)
+				.then( f => {
+					f = f.map(e => e.mail_to);
+					check.verifyUsers(f).then( checkMails => {
+						mailer.invite_user({ uid: r[index].user_id, mailList, category: 'gmail_auth' })
+						return recursive(index + 1);
+					});
+				})
+			} 
+		};
+		recursive(0);
+	})
+}
+
+sendInvitationToGmailContacts();
