2d44cb3f58f7b6b564949968c406bed4afab0cdc
jayho1 <lionfiercely_force@hotmail.fr>
2017-09-01 11:28:24 +0200

    fixed article update issue

--- app/models/article.js
+++ app/models/article.js
@@ -4,31 +4,51 @@ const { db, TABLES } = require('./index'),
 
 // ------------------ Local helpers ------------------
 
-const getTagsFromArticleId = (data, id) =>
-	db
-		.select('t.name', 'ta.id as tagged_id', 't.id')
-		.from(TABLES.ARTICLE_TAGS + ' as t')
-		.join(TABLES.TAG_ARTICLES + ' as ta', 'ta.tag_id', 't.id')
-		.where('ta.article_id', id)
-		.then(r => [r, data]);
+// const getTagsFromArticleId = (data, id) =>
+// 	db
+// 		.select('t.name', 'ta.id as tagged_id', 't.id')
+// 		.from(TABLES.ARTICLE_TAGS + ' as t')
+// 		.join(TABLES.TAG_ARTICLES + ' as ta', 'ta.tag_id', 't.id')
+// 		.where('ta.article_id', id)
+// 		.then(r => [r, data]);
 
-const removeOldTag = ([res, data]) => {
-	const tomap = _.difference(res.map(e => e.id), data.tags);
-	const fromdb = res.map(e => e.tagged_id);
-	const toReturn = _.difference(data.tags, res.map(e => e.id));
-	let toRemove = [];
-	res.forEach(el => {
-		tomap.forEach(r => {
-			if (el.id == r) toRemove.push(el.tagged_id);
-		});
-	});
-	if (toRemove.length) {
-		return db(TABLES.TAG_ARTICLES)
-			.del()
-			.whereIn('id', toRemove)
-			.then(r => toReturn);
-	}
+// const removeOldTag = ([res, data]) => {
+// 	const tomap = _.difference(res.map(e => e.id), data.tags);
+// 	const fromdb = res.map(e => e.tagged_id);
+// 	const toReturn = _.difference(data.tags, res.map(e => e.id));
+// 	let toRemove = [];
+// 	res.forEach(el => {
+// 		tomap.forEach(r => {
+// 			if (el.id == r) toRemove.push(el.tagged_id);
+// 		});
+// 	});
+// 	if (toRemove.length) {
+// 		return db(TABLES.TAG_ARTICLES)
+// 			.del()
+// 			.whereIn('id', toRemove)
+// 			.then(r => toReturn);
+// 	}
+// };
+
+const removeOldTag = (article_id) => {
+	return db(TABLES.TAG_ARTICLES)
+		.del()
+		.where('article_id', article_id)
 };
+
+const updateTags = (article_id, data) => {
+	return db(TABLES.ARTICLE_TAGS)
+		.select('id')
+		.whereIn('name', data)
+		.then(r => {
+			r.forEach(e => {
+				db(TABLES.TAG_ARTICLES)
+					.insert({ article_id: article_id, tag_id: e.id})
+					.return()
+			});
+			return ;
+		})
+}
 
 // ------------------ Main methods ------------------
 
@@ -49,7 +69,6 @@ exports.createArticle = data => {
 		});
 		return db(TABLES.ARTICLES).insert(i_data).then(([id]) => {
 			data.tags.forEach((el, i) => {
-				console.log(y[el]);
 				x.push(
 					db(TABLES.TAG_ARTICLES).insert({
 						article_id: id,
@@ -110,9 +129,10 @@ exports.removeArticle = (id, uid) => {
 exports.updateArticle = (data, id) => {
 	let x = [];
 	const i_data = {};
-	if (data.title) i_data.title = data.title;
-	if (data.text) i_data.text = data.text;
-	if (data.picture) i_data.picture = data.picture;
+	if (data.title) i_data.title 			= data.title;
+	if (data.text) i_data.text 				= data.text;
+	if (data.picture) i_data.picture 		= data.picture;
+	if (data.read_time) i_data.read_time 	= data.read_time
 
 	return h.admin(TABLES.ARTICLES, id, data.uid).then(([r, r1]) => {
 		if (!r.length || !r1.length || !data.tags) return 'Not an admin';
@@ -120,16 +140,9 @@ exports.updateArticle = (data, id) => {
 			return db(TABLES.ARTICLES)
 				.update(i_data)
 				.where('id', id)
-				.then(() => getTagsFromArticleId(data, id))
-				.then(removeOldTag)
+				.then(removeOldTag(id))
 				.then(res => {
-					res.forEach((el, i) => {
-						x.push(
-							db(TABLES.TAG_ARTICLES)
-								.insert({ article_id: id, tag_id: el })
-								.return()
-						);
-					});
+					updateTags(id, data.tags);
 					return Promise.all(x).then(() => ['Finished']);
 				});
 		}
