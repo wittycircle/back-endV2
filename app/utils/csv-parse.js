b0c2ddf9ff03692ef2a9823de3659e0ab7e7fe19
jayho1 <lionfiercely_force@hotmail.fr>
2017-10-24 11:23:23 +0200

    update

--- app/utils/csv-parse.js
+++ app/utils/csv-parse.js
@@ -2,41 +2,49 @@ const fs = require('fs');
 const csv = require("fast-csv");
 const { db, TABLES } = require('../models/index');
 
-let stream = fs.createReadStream('/home/ubuntu/WittycircleV2/Bulk_Invitation_Csv/Global_Cultural_Entrepreneurship_FG_out.csv');
+let stream;
+let argv3 = process.argv[3];
+
+if (process.argv[2] !== 'prod')
+	stream = fs.createReadStream('/Users/jayho/Desktop/Wittycircle/back-endV2/Bulk_Invitation_Csv/' + argv3 + '.csv');
+else
+	stream = fs.createReadStream('/home/ubuntu/WittycircleV2/Bulk_Invitation_Csv/' + argv3 + '.csv');
 
 let array = [];
 
 const saveEmail = (emails) => {
 	db('facebook_group_emails').insert(emails)
-	.then(r => { console.log(r) });
+	.then(r => { console.log('INSERT EMAILS DONE !') });
 };
 
-const removeLow = (emails) => {
-	db('facebook_group_emails')
-	.whereIn('email', emails)
-	.del()
-    .then(r => { console.log(r) });
-}
+// const removeLow = (emails) => {
+// 	db('facebook_group_emails')
+// 	.whereIn('email', emails)
+// 	.del()
+//     .then(r => { console.log(r) });
+// }
 
-const setCategory = (emails) => {
-	db('facebook_group_emails')
-	.whereIn('email', emails)
-	.update({
-		group_name: 'Global_Cultural_Entrepreneurship_FG'
-	}).then(r => { console.log(r) })
-}
+// const setCategory = (emails) => {
+// 	db('facebook_group_emails')
+// 	.whereIn('email', emails)
+// 	.update({
+// 		group_name: 'Global_Cultural_Entrepreneurship_FG'
+// 	}).then(r => { console.log(r) })
+// }
 
 const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 let csvStream = csv()
     .on("data", function(data){
-    	if (data[6] && data[6] !== 'low')
-          array.push({ email: data[5] });
+    	if (data[6] && data[6] !== 'low') {
+    		if (reg.test(data[5]))
+        		array.push({ email: data[5], group_name: argv3 });
+    	}
     })
     .on("end", function(){
-    	setCategory(array.map(e => e.email))
+    	// setCategory(array.map(e => e.email))
     	// removeLow(array.map(e => e.email ));
-	// saveEmail(array);
+		saveEmail(array);
     });
 
 stream.pipe(csvStream);
