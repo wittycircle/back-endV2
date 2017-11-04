f299715968965220c0b5e45a9e1c13f6d15cc217
Raphael BAUM <rbaum@e1r12p13.42.fr>
2017-11-04 19:22:08 +0100

    suggestion profile update according to profile.about and openings.status

--- app/models/suggestions/profiles.js
+++ app/models/suggestions/profiles.js
@@ -1,80 +1,111 @@
 const { db, TABLES } = require('../index'),
-h = require('../helper'),
-sh = require('./side_help');
+  h = require('../helper'),
+  sh = require('./side_help');
 
 const alreadySuggestedProfiles = projectId => {
-	return db(TABLES.SUG_PROFILES)
-	.select('user_id')
-	.where('project_id', projectId)
-	.map(e => e.user_id);
+  return db(TABLES.SUG_PROFILES)
+    .select('user_id')
+    .where('project_id', projectId)
+    .map(e => e.user_id);
 };
 
 module.exports.suggestProfiles = (projectId, profiles) => {
-	let o = profiles.map(e => ({ user_id: e, project_id: projectId }));
-	return db(TABLES.SUG_PROFILES).insert(o);
+  let o = profiles.map(e => ({ user_id: e, project_id: projectId }));
+  return db(TABLES.SUG_PROFILES).insert(o);
 };
 
-const getMatchingProfiles = (neededSkills, alreadySugested, projectLocation = []) => {
+const realStatus = status => {
+  const projectToProfile = {
+    'for help': ['to meet smart people', 'for part time collaboration'],
+    'for feedback': [
+      'to meet smart people',
+      'for part time collaboration',
+      'for a full time position',
+      "to share what I'm working on",
+      'for an internship'
+    ],
+    'to hire someone': [
+      'for a full time position',
+      'for part time collaboration'
+    ],
+    'to hire an intern': ['for an internship'],
+    'for a cofounder': ['to meet smart people']
+  };
+  let x = [];
+  status.forEach(s => {
+    if (s in projectToProfile) x = x.concat(projectToProfile[s]);
+  });
+  return x.filter((item, pos, self) => self.indexOf(item) == pos);
+};
 
-	alreadySugested[alreadySugested.length] = projectLocation[0].puid;
+const getMatchingProfiles = (
+  status,
+  neededSkills,
+  alreadySugested,
+  projectLocation = []
+) => {
+  alreadySugested[alreadySugested.length] = projectLocation[0].puid;
 
-	const user_skills = db
-	.select(
-		'us.user_id as usid',
-		db.raw(`GROUP_CONCAT(DISTINCT sk.name) as skills`))
-	.from((TABLES.USER_SKILLS) + ' as us')
-	.leftJoin(TABLES.SKILLS + ' as sk', 'sk.id', 'us.skill_id')
-	.groupBy('us.user_id')
-	.as('usk')
+  const user_skills = db
+    .select(
+      'us.user_id as usid',
+      db.raw(`GROUP_CONCAT(DISTINCT sk.name) as skills`)
+    )
+    .from(TABLES.USER_SKILLS + ' as us')
+    .leftJoin(TABLES.SKILLS + ' as sk', 'sk.id', 'us.skill_id')
+    .groupBy('us.user_id')
+    .as('usk');
 
-	let query = db
-	.distinct(
-		'p.*',
-		'nl.name as network',
-		'usk.skills',
-		h.format_location
-// db.raw('GROUP_CONCAT(distinct us.skill_id) as skillId'),
-// db.raw('GROUP_CONCAT(DISTINCT s.name) as skillName')
-	)
-	.from(h.spe_profile({}))
-	.join(TABLES.USER_SKILLS + ' as us', 'us.user_id', 'p.uid')
-	.join(TABLES.SKILLS + ' as s', 's.id', 'us.skill_id')
-	.join(TABLES.LOCATION + ' as loc', 'loc.id', 'p.loc_id')
-	.join(TABLES.NETWORKS_LIST + ' as nl', 'nl.id', 'p.network_id')
-	.leftJoin(user_skills, 'usk.usid', 'p.uid')
-	.whereIn('skill_id', neededSkills)
-	.whereNotIn('p.uid', alreadySugested)
-	.groupBy('p.id')
-	.orderByRaw(`field(loc.city, '${projectLocation[0].city}') desc, 
+  let query = db
+    .distinct(
+      'p.*',
+      'nl.name as network',
+      'usk.skills',
+      h.format_location
+      // db.raw('GROUP_CONCAT(distinct us.skill_id) as skillId'),
+      // db.raw('GROUP_CONCAT(DISTINCT s.name) as skillName')
+    )
+    .from(h.spe_profile({}))
+    .join(TABLES.USER_SKILLS + ' as us', 'us.user_id', 'p.uid')
+    .join(TABLES.SKILLS + ' as s', 's.id', 'us.skill_id')
+    .join(TABLES.LOCATION + ' as loc', 'loc.id', 'p.loc_id')
+    .join(TABLES.NETWORKS_LIST + ' as nl', 'nl.id', 'p.network_id')
+    .leftJoin(user_skills, 'usk.usid', 'p.uid')
+    .whereIn('skill_id', neededSkills)
+    .whereIn('p.about', status)
+    .whereNotIn('p.uid', alreadySugested)
+    .groupBy('p.id').orderByRaw(`field(loc.city, '${projectLocation[0]
+    .city}') desc,
 		field(loc.state, '${projectLocation[0].state}') desc,
 		field(loc.country, '${projectLocation[0].country}') desc`);
 
-	return query;
+  return query;
 };
 
-// module.exports.getMatchingProjects = getMatchingProjects;
+const matchProfilesToProject = (module.exports.matchProfilesToProject = projectId => {
+  return Promise.all([
+    sh.skillsFromProjectId(projectId),
+    sh.locationFromProjectId(projectId),
+    alreadySuggestedProfiles(projectId)
+  ]).then(r => {
+    const neededSkills = r[0].skillId.split(',');
+    const status = realStatus(r[0].openingStatus.split(','));
+    const projectLocation = r[1];
+    const alreadySugested = r[2];
 
-const matchProfilesToProject = (module.exports.matchProfilesToProject) = projectId => {
-
-	return Promise.all([
-		sh.skillsFromProjectId(projectId),
-		sh.locationFromProjectId(projectId),
-		alreadySuggestedProfiles(projectId),
-		]).then(r => {
-			let neededSkills 	= r[0].skillId.split(',');
-			let projectLocation = r[1];
-			let alreadySugested = r[2];
-
-			return getMatchingProfiles(neededSkills, alreadySugested, projectLocation).then(r => {
-				if (!r.length || r.length < 5) {
-					return sh.expandedSkills(neededSkills).then(expandedNeeds => {
-						return getMatchingProfiles(expandedNeeds, alreadySugested);
-					});
-				} else {
-					return r;
-				}
-			});
-		});
-	};
-
-// matchProfilesToProject(3);
+    return getMatchingProfiles(
+      status,
+      neededSkills,
+      alreadySugested,
+      projectLocation
+    ).then(r => {
+      if (!r.length || r.length < 5) {
+        return sh.expandedSkills(neededSkills).then(expandedNeeds => {
+          return getMatchingProfiles(status, expandedNeeds, alreadySugested);
+        });
+      } else {
+        return r;
+      }
+    });
+  });
+});
--- app/services/mailer/suggest_mails/suggestion_profile.js
+++ app/services/mailer/suggest_mails/suggestion_profile.js
@@ -4,100 +4,112 @@ const h = require('../../../models/helper'); //NIK
 const suggestions = require('../../../models/suggestions');
 const { db, TABLES } = require('../../../models/index');
 
-const profile_bloc = (name, picture, description, location, network, skills, username) => {
-	return `<div class="main-class"><div class="bloc1" style="display: inline-block;"><div class="first-container" style="margin-bottom: 10px;"><img style="border-radius: 50%; width: 50px;" src="${picture}" alt="profile_picture"></div>
-<div class="second-container" style="margin: 0 10px 10px 10px;"><h2 style="margin: 0; font-family: Arial, Tahoma, Vernada; font-weight: bold; font-size: 14px; color: #323232; margin-bottom: 5px;">${name}</h2><h3 class="space-normal" style="margin: 0; font-family: Tahoma; font-weight: normal; font-size: 12px; color: #697178; margin-bottom: 5px; ">${wm.shortenerText(description, true, 76, ' ...')}</h3>
-<div class="location" style="margin-bottom: 10px;">${wm.location_bloc(wm.location(location))}${wm.network_bloc(network)}</div><div class="skill-list space-normal" style="margin-top: 10px"><ul class="noPadding">${wm.skills_bloc2(skills)}
-</ul></div></div></div><div class="third-container"><a href="https://www.wittycircle.com/${username}" class="button">View profile</a></div></div>`
+const profile_bloc = (
+  name,
+  picture,
+  description,
+  location,
+  network,
+  skills,
+  username
+) => {
+  return `<div class="main-class"><div class="bloc1" style="display: inline-block;"><div class="first-container" style="margin-bottom: 10px;"><img style="border-radius: 50%; width: 50px;" src="${picture}" alt="profile_picture"></div>
+<div class="second-container" style="margin: 0 10px 10px 10px;"><h2 style="margin: 0; font-family: Arial, Tahoma, Vernada; font-weight: bold; font-size: 14px; color: #323232; margin-bottom: 5px;">${name}</h2><h3 class="space-normal" style="margin: 0; font-family: Tahoma; font-weight: normal; font-size: 12px; color: #697178; margin-bottom: 5px; ">${wm.shortenerText(
+    description,
+    true,
+    76,
+    ' ...'
+  )}</h3>
+<div class="location" style="margin-bottom: 10px;">${wm.location_bloc(
+    wm.location(location)
+  )}${wm.network_bloc(
+    network
+  )}</div><div class="skill-list space-normal" style="margin-top: 10px"><ul class="noPadding">${wm.skills_bloc2(
+    skills
+  )}
+</ul></div></div></div><div class="third-container"><a href="https://www.wittycircle.com/${username}" class="button">View profile</a></div></div>`;
 };
 
-const fillSub = (b) => {
-	return profile_bloc(
-		b.fullName,
-		b.picture,
-		b.description,
-		b,
-		b.network,
-		b.skills,
-		b.username
-	);
+const fillSub = b => {
+  return profile_bloc(
+    b.fullName,
+    b.picture,
+    b.description,
+    b,
+    b.network,
+    b.skills,
+    b.username
+  );
 };
 
 const send_mail = (project, profiles) => {
-	console.log('SEND MAIL CALLED');
-	
-	let mail = new helper.Mail();
-	wm.from(mail, 'notifications@wittycircle.com', 'Wittycircle');
-	wm.content(mail);
-	wm.reply(mail, 'notifications@wittycircle.com');
-	mail.setTemplateId(TEMPLATES.suggestions_profile);
+  let mail = new helper.Mail();
+  wm.from(mail, 'notifications@wittycircle.com', 'Wittycircle');
+  wm.content(mail);
+  wm.reply(mail, 'notifications@wittycircle.com');
+  mail.setTemplateId(TEMPLATES.suggestions_profile);
 
+  const category = new helper.Category('suggestion_profiles');
+  mail.addCategory(category);
 
-	const category = new helper.Category('suggestion_profiles');
-	mail.addCategory(category);
+  let laString = '';
+  let pers = new helper.Personalization();
+  let subject = 'Those people could help you with *|PR_TITLE|*';
+  let sub = {
+    '*|PF_NAME|*': project.first_name,
+    '*|PR_TITLE|*': project.title,
+    MAIL: project.email
+  };
+  profiles.forEach((b, i) => {
+    if (i < 3) laString += fillSub(b);
+  });
+  sub['*|PROFILES_BLOC|*'] = laString;
 
-	let laString = '';
-	let pers = new helper.Personalization();
-	let subject = 'Those people could help you with *|PR_TITLE|*';
-	let sub = {
-		'*|PF_NAME|*' 		: project.first_name,
-		'*|PR_TITLE|*' 		: project.title,
-		MAIL: project.email
-	};
-	profiles.forEach((b, i) => {
-		if (i < 3) laString += fillSub(b);
-	});
-	sub['*|PROFILES_BLOC|*'] = laString;
+  wm.subject(pers, subject);
+  wm.to(pers, project.email);
 
-	wm.subject(pers, subject);
-	wm.to(pers,  project.email);
+  wm.substitutions(pers, sub);
+  mail.addPersonalization(pers);
+  wm.send(mail, 'suggestion_profiles');
 
-	wm.substitutions(pers, sub);
-	mail.addPersonalization(pers);
-	wm.send(mail, 'suggestion_profiles');
-
-	return null
-
+  return null;
 };
 
-
 const saveSentData = (project, profiles) => {
-	let data = [];
+  let data = [];
 
-	profiles.forEach(e => {
-		data.push({ user_id: e.uid, project_id: project.id })
-	});
+  profiles.forEach(e => {
+    data.push({ user_id: e.uid, project_id: project.id });
+  });
 
-	db(TABLES.SUG_PROFILES).insert(data).then(r => { return null })
-}
+  db(TABLES.SUG_PROFILES)
+    .insert(data)
+    .then(r => {
+      return null;
+    });
+};
 
 const suggestionProfileToProject = () => {
-
-	db
-	.select(
-		'pr.id',
-		'pr.title',
-		'u.email',
-		'p.first_name',
-		'p.last_name'
-	)
-	.from(TABLES.PROJECTS + ' as pr')
-	.join(TABLES.USERS + ' as u', 'u.id', 'pr.user_id')
-	.join(TABLES.PROFILES + ' as p', 'p.user_id', 'pr.user_id')
-	.whereRaw('pr.picture is not null && pr.picture != "" && pr.description is not null && pr.description != ""')
-	.then(r => {
-		r.forEach((e, i) => {
-			if (e.email.indexOf('witty') < 0) {
-				suggestions.matchProfilesToProject(e.id)
-					.then(profiles => {	
-						let profilesSent = profiles.splice(0, 3)
-						send_mail(e, profilesSent);
-						saveSentData(e, profilesSent);
-					});
-			}
-		});
-		console.log("PROFILE DONE!");
-	})
+  db
+    .select('pr.id', 'pr.title', 'u.email', 'p.first_name', 'p.last_name')
+    .from(TABLES.PROJECTS + ' as pr')
+    .join(TABLES.USERS + ' as u', 'u.id', 'pr.user_id')
+    .join(TABLES.PROFILES + ' as p', 'p.user_id', 'pr.user_id')
+    .whereRaw(
+      'pr.picture is not null && pr.picture != "" && pr.description is not null && pr.description != ""'
+    )
+    .then(r => {
+      r.forEach((e, i) => {
+        if (e.email.indexOf('witty') < 0) {
+          suggestions.matchProfilesToProject(e.id).then(profiles => {
+            let profilesSent = profiles.splice(0, 3);
+            send_mail(e, profilesSent);
+            saveSentData(e, profilesSent);
+          });
+        }
+      });
+      console.log('PROFILE DONE!');
+    });
 };
 
 // suggestionProfileToProject();
