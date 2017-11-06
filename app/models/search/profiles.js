7dd8e0c94c0f684fde9e239dc3fe2513c8906b94
Raphael BAUM <rbaum@e1r12p7.42.fr>
2017-11-06 20:03:07 +0100

    :sparkles: ?

--- app/models/search/profiles.js
+++ app/models/search/profiles.js
@@ -1,145 +1,156 @@
 'use strict';
 
 const { db, TABLES } = require('../index'),
-  h = require('../helper'),
-  data = require('./data'),
-  _ = require('lodash');
+    h = require('../helper'),
+    data = require('./data'),
+    _ = require('lodash');
 
 // ------------------ DATA PROFILE SEARCH ---------------
 const profile_array = [
-  'p.id',
-  'p.user_id',
-  'p.picture',
-  'p.first_name',
-  'p.about',
-  'p.cover_picture',
-  'p.description',
-  'nl.name as network',
-  h.format_location,
-  h.fullname,
-  'u.username'
+    'p.id',
+    'p.user_id',
+    'p.picture',
+    'p.first_name',
+    'p.about',
+    'p.cover_picture',
+    'p.description',
+    'nl.name as network',
+    h.format_location,
+    h.fullname,
+    'u.username'
 ];
 
 const ret_array = [
-  'sort.weight',
-  'fullName',
-  'username',
-  'rank',
-  'first_name',
-  'sort.id as user_id',
-  'p.id',
-  'picture',
-  'foli',
-  'cover_picture',
-  'about',
-  'description',
-  'network',
-  'city',
-  'state',
-  'country',
-  'follower',
-  'following',
-  'skills'
+    'sort.weight',
+    'fullName',
+    'username',
+    'rank',
+    'first_name',
+    'sort.id as user_id',
+    'p.id',
+    'picture',
+    'foli',
+    'cover_picture',
+    'about',
+    'description',
+    'network',
+    'city',
+    'state',
+    'country',
+    'follower',
+    'following',
+    'skills'
 ];
 
 const exp = db
-  .select('user_id')
-  .from(TABLES.USER_EXPERIENCES)
-  .as('e');
+    .select('user_id')
+    .from(TABLES.USER_EXPERIENCES)
+    .as('e');
 
 const following = db
-  .select('user_id')
-  .count('user_id as total')
-  .from(TABLES.USER_FOLLOWERS)
-  .groupBy('user_id')
-  .as('ssu');
+    .select('user_id')
+    .count('user_id as total')
+    .from(TABLES.USER_FOLLOWERS)
+    .groupBy('user_id')
+    .as('ssu');
 const follower = db
-  .select('followed')
-  .count('followed as MA')
-  .from(TABLES.USER_FOLLOWERS)
-  .groupBy('followed')
-  .as('su');
+    .select('followed')
+    .count('followed as MA')
+    .from(TABLES.USER_FOLLOWERS)
+    .groupBy('followed')
+    .as('su');
 const ifo = db
-  .distinct('followed', 'user_id')
-  .from(TABLES.USER_FOLLOWERS)
-  .as('ifo');
+    .distinct('followed', 'user_id')
+    .from(TABLES.USER_FOLLOWERS)
+    .as('ifo');
 
 // ------------------ Profile ------------------
 module.exports = selector => {
-  // ---------  Main query -------
-  const skills = db
-    .select('user_id', 's.*')
-    .from(TABLES.USER_SKILLS + ' as us')
-    .join(h.magicSkills(selector.cats), 'us.skill_id', 's.id')
-    .as('s');
-  const sortCardProfile = db
-    .select([
-      's.weight',
-      'u.id',
-      'r.rank as rank',
-      db.raw('GROUP_CONCAT(ifo.user_id) as foli'),
-      db.raw('IFNULL(total, 0) as following'),
-      db.raw('IFNULL (MA, 0) as follower'),
-      db.raw('GROUP_CONCAT(DISTINCT s.name) as skills'),
-      db.raw('GROUP_CONCAT(DISTINCT s.catName) as catNames')
-    ])
-    .from(TABLES.USERS + ' as u')
-    .leftJoin(skills, 'u.id', 's.user_id')
-    .leftJoin(TABLES.RANK + ' as r', 'u.id', 'r.user_id') //leftJoin to get those without rank [All users will have a rank?]
-    .leftJoin(following, 'ssu.user_id', 'u.id')
-    .leftJoin(follower, 'su.followed', 'u.id')
-    .leftJoin(ifo, 'ifo.followed', 'u.id')
-    .groupBy('u.id')
-    .as('sort');
+    // ---------  Main query -------
+    const skills = db
+        .distinct(
+            'user_id',
+            's.name',
+            's.Catname',
+            db.raw('SUM(DISTINCT s.weight) as weight')
+        )
+        .from(TABLES.USER_SKILLS + ' as us')
+        .join(h.magicSkills(selector.cats), 'us.skill_id', 's.id')
+        .groupBy('user_id')
+        .orderBy('weight', 'desc')
+        .limit(10)
+        .as('s');
 
-  //  Profile stuff [hydrate with infos, join on q below]
+    const sortCardProfile = db
+        .select([
+            's.weight',
+            'u.id',
+            'r.rank as rank',
+            db.raw('GROUP_CONCAT(ifo.user_id) as foli'),
+            db.raw('IFNULL(total, 0) as following'),
+            db.raw('IFNULL (MA, 0) as follower'),
+            db.raw('GROUP_CONCAT(DISTINCT s.name) as skills'),
+            db.raw('GROUP_CONCAT(DISTINCT s.catName) as catNames')
+        ])
+        .from(TABLES.USERS + ' as u')
+        .leftJoin(skills, 'u.id', 's.user_id')
+        .leftJoin(TABLES.RANK + ' as r', 'u.id', 'r.user_id') //leftJoin to get those without rank [All users will have a rank?]
+        .leftJoin(following, 'ssu.user_id', 'u.id')
+        .leftJoin(follower, 'su.followed', 'u.id')
+        .leftJoin(ifo, 'ifo.followed', 'u.id')
+        .groupBy('u.id')
+        .as('sort');
 
-  const profileStuff = location => {
-    return db
-      .select(profile_array.concat('p.creation_date'))
-      .from(TABLES.PROFILES + ' as p')
-      .join(TABLES.USERS + ' as u', 'p.user_id', 'u.id')
-      .join(TABLES.NETWORKS_LIST + ' as nl', 'p.network_id', 'nl.id')
-      .join(TABLES.LOCATION + ' as loc', 'loc.id', 'p.loc_id')
-      .where('p.description', '!=', 'NULL')
-      .andWhere('p.picture', '!=', 'NULL')
-      .andWhere('u.fake', '=', '0')
-      .as('p');
-  };
+    //  Profile stuff [hydrate with infos, join on q below]
 
-  let q = db
-    .select(ret_array)
-    .from(sortCardProfile)
-    .join(profileStuff(selector.location), 'sort.id', 'p.user_id')
-    .leftJoin(exp, 'e.user_id', 'sort.id')
-    .groupBy('sort.id')
-    .where('sort.rank', '>', '0'); //todo remove
+    const profileStuff = location => {
+        return db
+            .select(profile_array.concat('p.creation_date'))
+            .from(TABLES.PROFILES + ' as p')
+            .join(TABLES.USERS + ' as u', 'p.user_id', 'u.id')
+            .join(TABLES.NETWORKS_LIST + ' as nl', 'p.network_id', 'nl.id')
+            .join(TABLES.LOCATION + ' as loc', 'loc.id', 'p.loc_id')
+            .where('p.description', '!=', 'NULL')
+            .andWhere('p.picture', '!=', 'NULL')
+            .andWhere('u.fake', '=', '0')
+            .as('p');
+    };
 
-  // ******** ********  TRYING  ******** ********
+    let q = db
+        .select(ret_array)
+        .from(sortCardProfile)
+        .join(profileStuff(selector.location), 'sort.id', 'p.user_id')
+        .leftJoin(exp, 'e.user_id', 'sort.id')
+        .groupBy('sort.id')
+        .where('sort.rank', '>', '0'); //todo remove
 
-  let associated = {
-    location: () => h.addLocation('p', selector.location, q),
-    skills: () => q.whereRaw('weight IS NOT NULL').orderByRaw('sort.weight'),
-    network: () =>
-      q.orderByRaw(
-        `CASE WHEN network like "%${selector.network}%" THEN 1 else 2 END, network`
-      ),
-    about: () =>
-      q.orderByRaw(
-        'CASE WHEN about = "' + selector.about + '" THEN 1 else 2 END, about'
-      )
-  };
+    // ******** ********  TRYING  ******** ********
 
-  let mh = Object.keys(associated);
-  if (selector.priority) {
-    mh = mh.filter(e => e == selector.priority);
-    mh.unshift(selector.priority);
-  }
-  mh.forEach(e => {
-    if (e in selector) {
-      associated[e]();
+    let associated = {
+        location: () => h.addLocation('p', selector.location, q),
+        skills: () => q.whereRaw('weight IS NOT NULL').orderByRaw('sort.weight'),
+        network: () =>
+            q.orderByRaw(
+                `CASE WHEN network like "%${selector.network}%" THEN 1 else 2 END, network`
+            ),
+        about: () =>
+            q.orderByRaw(
+                'CASE WHEN about = "' +
+                    selector.about +
+                    '" THEN 1 else 2 END, about'
+            )
+    };
+
+    let mh = Object.keys(associated);
+    if (selector.priority) {
+        mh = mh.filter(e => e == selector.priority);
+        mh.unshift(selector.priority);
     }
-  });
+    mh.forEach(e => {
+        if (e in selector) {
+            associated[e]();
+        }
+    });
 
-  return q;
+    return q;
 };
--- app/models/search/projects.js
+++ app/models/search/projects.js
@@ -1,117 +1,122 @@
 const { db, TABLES } = require('../index'),
-  h = require('../helper'),
-  data = require('./data'),
-  _ = require('lodash');
+    h = require('../helper'),
+    data = require('./data'),
+    _ = require('lodash');
 
 // ------------------ Project ------------------
 
 module.exports = selector => {
-  const sub_members = db(TABLES.PROJECT_MEMBERS + ' as m')
-    .select('m.project_id', 'm.user_id')
-    .where('accepted', 1)
-    .as('m');
+    const sub_members = db(TABLES.PROJECT_MEMBERS + ' as m')
+        .select('m.project_id', 'm.user_id')
+        .where('accepted', 1)
+        .as('m');
 
-  const sub_category = db(TABLES.CATEGORIES + ' as c')
-    .select('c.id', 'c.name')
-    .as('c');
+    const sub_category = db(TABLES.CATEGORIES + ' as c')
+        .select('c.id', 'c.name')
+        .as('c');
 
-  const sub_openings = db(TABLES.PROJECT_OPENINGS + ' as o')
-    .select([
-      db.raw('GROUP_CONCAT(s.name) as tags'),
-      'o.project_id',
-      'status',
-      'weight'
-    ])
-    .leftJoin(TABLES.OPENING_TAGS + ' as ot', 'o.id', 'ot.opening_id')
-    .leftJoin(h.magicSkills(selector.cats), 'ot.skill_id', 's.id')
-    .groupBy('o.id')
-    .as('o');
+    const sub_openings = db(TABLES.PROJECT_OPENINGS + ' as o')
+        .select([
+            db.raw('GROUP_CONCAT(s.name) as tags'),
+            'o.project_id',
+            'status',
+            'weight'
+        ])
+        .leftJoin(TABLES.OPENING_TAGS + ' as ot', 'o.id', 'ot.opening_id')
+        .leftJoin(h.magicSkills(selector.cats), 'ot.skill_id', 's.id')
+        .groupBy('o.id')
+        .as('o');
 
-  let pr_array = [
-    'pr.id',
-    'pr.creation_date',
-    'pr.title',
-    'pr.description',
-    'pr.picture as picture',
-    'pr.status',
-    'pr.public_id',
-    'c.id as category_id',
-    'c.name as category_name',
-    'nl.name as network',
-    'p.picture as profile_picture',
-    'p.uid as user_id',
-    'openingStat',
-    db.raw('CONCAT (p.first_name, " ", p.last_name) as username'),
-    h.format_location
-  ];
+    let pr_array = [
+        'pr.id',
+        'pr.creation_date',
+        'pr.title',
+        'pr.description',
+        'pr.picture as picture',
+        'pr.status',
+        'pr.public_id',
+        'c.id as category_id',
+        'c.name as category_name',
+        'nl.name as network',
+        'p.picture as profile_picture',
+        'p.uid as user_id',
+        'openingStat',
+        'firstStatus',
+        db.raw('CONCAT (p.first_name, " ", p.last_name) as username'),
+        h.format_location
+    ];
 
-  const opening_counts = db
-    .select('project_id')
-    .count('status as openingStat')
-    .from('openings')
-    .groupBy('project_id')
-    .as('opc');
+    const opening_counts = db
+        .select('project_id', 'status as firstStatus')
+        .count('status as openingStat')
+        .from('openings')
+        .groupBy('project_id')
+        .as('opc');
 
-  const query = db
-    .distinct(pr_array)
-    .countDistinct('pl.id as followers')
-    .countDistinct('m.user_id as members')
-    .from(TABLES.PROJECTS + ' as pr')
-    .join(h.sub_profile, 'p.uid', 'pr.user_id')
-    .join(TABLES.NETWORKS_LIST + ' as nl', 'nl.id', 'p.network_id')
-    .join(TABLES.LOCATION + ' as loc', 'loc.id', 'pr.loc_id')
-    .join(sub_category, 'c.id', 'pr.category_id')
-    .leftJoin(TABLES.PROJECT_LIKES + ' as pl', 'pl.project_id', 'pr.id')
-    .leftJoin(sub_members, 'm.project_id', 'pr.id')
-    .leftJoin(opening_counts, 'opc.project_id', 'pr.id')
-    .where('pr.project_visibility', 1)
-    .whereRaw('pr.picture <> ""')
-    .groupBy('pr.id', 'nl.id', 'p.id');
+    const query = db
+        .distinct(pr_array)
+        .countDistinct('pl.id as followers')
+        .countDistinct('m.user_id as members')
+        .from(TABLES.PROJECTS + ' as pr')
+        .join(h.sub_profile, 'p.uid', 'pr.user_id')
+        .join(TABLES.NETWORKS_LIST + ' as nl', 'nl.id', 'p.network_id')
+        .join(TABLES.LOCATION + ' as loc', 'loc.id', 'pr.loc_id')
+        .join(sub_category, 'c.id', 'pr.category_id')
+        .leftJoin(TABLES.PROJECT_LIKES + ' as pl', 'pl.project_id', 'pr.id')
+        .leftJoin(sub_members, 'm.project_id', 'pr.id')
+        .leftJoin(opening_counts, 'opc.project_id', 'pr.id')
+        .where('pr.project_visibility', 1)
+        .whereRaw('pr.picture <> ""')
+        .groupBy('pr.id', 'nl.id', 'p.id');
 
-  // ******** ********  TRING  ******** ********
-  if (selector.skills || selector.opening) {
-    query.leftJoin(sub_openings, 'o.project_id', 'pr.id');
-  }
-  let associated = {
-    uid: () =>
-      pr_array.push(
-        db.raw(
-          `GROUP_CONCAT(DISTINCT IF(pl.user_id = ${selector.uid}, true, null)) as follow`
-        )
-      ),
-    network: () =>
-      query.orderByRaw(
-        `CASE WHEN nl.name like "%${selector.network}%" THEN 1 else 2 END`
-      ),
-    skills: () => {
-      pr_array.push('weight');
-      query.whereRaw('weight IS NOT NULL');
-      query.orderByRaw('weight');
-    },
-    location: () => h.addLocation('loc', selector.location, query),
-    opening: () => {
-      query.orderByRaw(
-        'CASE WHEN  o.status = "' + selector.opening + '" THEN 1 ELSE 2 END'
-      );
-    },
-    category: () =>
-      query.orderByRaw('(c.name = "' + selector.category + '") DESC'),
-    status: () =>
-      query.orderByRaw(
-        'CASE WHEN pr.status LIKE "%' + selector.status + '%" THEN 1 else 2 END'
-      )
-  };
-
-  let mh = Object.keys(associated);
-  if (selector.priority) {
-    mh = mh.filter(e => e !== selector.priority);
-    mh.unshift(selector.priority);
-  }
-  mh.forEach(e => {
-    if (e in selector) {
-      associated[e]();
+    // ******** ********  TRING  ******** ********
+    if (selector.skills || selector.opening) {
+        query.leftJoin(sub_openings, 'o.project_id', 'pr.id');
     }
-  });
+    let associated = {
+        uid: () =>
+            pr_array.push(
+                db.raw(
+                    `GROUP_CONCAT(DISTINCT IF(pl.user_id = ${selector.uid}, true, null)) as follow`
+                )
+            ),
+        network: () =>
+            query.orderByRaw(
+                `CASE WHEN nl.name like "%${selector.network}%" THEN 1 else 2 END`
+            ),
+        skills: () => {
+            pr_array.push('weight');
+            query.whereRaw('weight IS NOT NULL');
+            query.orderByRaw('weight');
+        },
+        location: () => h.addLocation('loc', selector.location, query),
+        opening: () => {
+            query.orderByRaw(
+                'CASE WHEN  o.status = "' +
+                    selector.opening +
+                    '" THEN 1 ELSE 2 END'
+            );
+        },
+        category: () =>
+            query.orderByRaw('(c.name = "' + selector.category + '") DESC'),
+        status: () =>
+            query.orderByRaw(
+                'CASE WHEN pr.status LIKE "%' +
+                    selector.status +
+                    '%" THEN 1 else 2 END'
+            )
+    };
 
-  return query;
+    let mh = Object.keys(associated);
+    if (selector.priority) {
+        mh = mh.filter(e => e !== selector.priority);
+        mh.unshift(selector.priority);
+    }
+    mh.forEach(e => {
+        if (e in selector) {
+            associated[e]();
+        }
+    });
+
+    return query;
 };
