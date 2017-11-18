f36781079a35e8ba1cda49fe9608ebd3d17cb5ea
Jay Ho <jayho@imac-de-jay.home>
2017-11-18 11:45:51 +0100

    update

--- app/services/algolia.js
+++ app/services/algolia.js
@@ -18,31 +18,63 @@ const init = (module.exports.initPeopleAndProjectIndex = () => {
 
 	let storage = {};
 
-	Promise.all([
-		db(h.spe_profile({ 'u.fake': 0 }).select('u.fake'))
-			.join(TABLES.LOCATION + ' as loc', 'loc.id', 'p.loc_id')
-			.select('p.*', h.format_location)
-			.then(profiles => {
-				storage.profiles = profiles;
-			    client.deleteIndex('Users', err => {
-					people.addObjects(profiles.filter(p => p.fullName && p.picture));
-				});
-			}),
-		db(TABLES.PROJECTS + ' as p')
-			.select('*')
-			.innerJoin('categories', 'categories.id', 'p.category_id')
-			.where('p.project_visibility', 1)
-			.then(projects => {
-				storage.projects = projects;
-				client.deleteIndex('Projects', err => {
-					project.addObjects(projects);
-				});
-			})
-	]).then(() =>
-		client.deleteIndex('PAndP', err =>
-				   pandp.addObjects([...storage.profiles, ...storage.projects])
-				   )
-		);
+	const profiles = db.select(
+			'p.first_name',
+			'p.last_name',
+			db.raw('CONCAT(p.first_name, " ", p.last_name) as fullName'),
+			'p.picture',
+			'p.about',
+			'p.description',
+			'loc.city',
+			'loc.state',
+			'loc.country',
+			'u.username',
+			'u.invite_link',
+			'u.fake'
+		)
+		.from(TABLES.USERS + ' as u')
+		.join(TABLES.PROFILES + ' as p', 'p.user_id', 'u.id')
+		.join(TABLES.LOCATION + ' as loc', 'loc.id', 'p.loc_id')
+		.where('u.fake', 0)
+		.andWhereRaw("u.email not like '%@witty.com%'")
+		.havingRaw('p.picture is not null AND p.picture != "" AND fullName is not null AND fullName != ""')
+		.orderBy('u.id');
+
+	const projects = db.select(
+			'pr.id',
+			'pr.public_id',
+			'pr.status',
+			'pr.title',
+			'ct.name as name',
+			'pr.description',
+			'pr.picture',
+			'pr.about',
+			'pr.logo',
+			'pr.project_visibility',
+			'pr.1st_description',
+			'pr.2nd_description',
+		)
+		.from(TABLES.PROJECTS + ' as pr')
+		.join(TABLES.CATEGORIES + ' as ct', 'ct.id', 'pr.category_id')
+		.where('pr.project_visibility', 1)
+		.orderBy('id')
+
+	Promise.all([profiles, projects])
+		.then((r) => {
+			client.deleteIndex('Users', err => {
+				people.addObjects(r[0]);
+				console.log('Add users done !');
+			});
+			client.deleteIndex('Projects', err => {
+				project.addObjects(r[1]);
+				console.log('Add projects done !');
+			});
+			client.deleteIndex('PAndP', err => {
+				pandp.addObjects([...r[1], ...r[0]]);
+				console.log('Add users and projects done !');
+			});
+		})
 });
+
 init();
 
